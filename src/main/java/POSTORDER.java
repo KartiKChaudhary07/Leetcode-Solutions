6ef23feaa86e7cee8d9eb503d628624e0e82d79d
KartiKChaudhary07 <kartik9983chaudhary@gmail.com>
2024-03-17 18:13:12 +0530

    new commit

---
+++ src/main/java/POSTORDER.java
@@ -0,0 +1,15 @@
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+class POSTORDER {
+    List<Integer> list=new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root){
+        if(root==null) return list;
+
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        list.add(root.val);
+        return list;
+    }
+}
