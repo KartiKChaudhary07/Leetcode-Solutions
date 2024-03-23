bb73c1a092cc89646618b4ce4fefa873354ea2cb
Kartik Chaudhary <117715537+KartiKChaudhary07@users.noreply.github.com>
2024-03-23 22:45:50 +0530

    Add files via upload
    new commit

---
+++ newSolution.java
@@ -0,0 +1,18 @@
+
+
+class newSolution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+
+        boolean leftSum = hasPathSum(root.left, targetSum - root.val);
+        boolean rightSum = hasPathSum(root.right, targetSum - root.val);
+
+        return leftSum || rightSum;
+    }
+}
