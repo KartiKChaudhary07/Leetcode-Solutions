c16825793103a3a4d3abb81f0fb48024038c2dd1
Kartik Chaudhary <117715537+KartiKChaudhary07@users.noreply.github.com>
2024-03-27 07:05:30 +0530

    Create Solution03.java
    new Solution

---
+++ Solution03.java
@@ -0,0 +1,36 @@
+/*1450. Number of Students Doing Homework at a Given Time
+
+Given two integer arrays startTime and endTime and given an integer queryTime.
+
+The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
+
+Return the number of students doing their homework at time queryTime. More formally, return the number of students where queryTime lays in the interval [startTime[i], endTime[i]] inclusive.
+
+ 
+
+Example 1:
+
+Input: startTime = [1,2,3], endTime = [3,2,7], queryTime = 4
+Output: 1
+Explanation: We have 3 students where:
+The first student started doing homework at time 1 and finished at time 3 and wasn't doing anything at time 4.
+The second student started doing homework at time 2 and finished at time 2 and also wasn't doing anything at time 4.
+The third student started doing homework at time 3 and finished at time 7 and was the only student doing homework at time 4.
+Example 2:
+
+Input: startTime = [4], endTime = [4], queryTime = 4
+Output: 1
+Explanation: The only student was doing their homework at the queryTime.*/
+
+
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int ans = 0;
+        for(int i=0;i<startTime.length;i++){
+            if(startTime[i] <= queryTime && endTime[i] >= queryTime){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
