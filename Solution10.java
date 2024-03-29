99c12221b5784061051c29ed5f82c14c3a4c987a
Kartik Chaudhary <117715537+KartiKChaudhary07@users.noreply.github.com>
2024-03-29 07:53:30 +0530

    Create Solution10.java

---
+++ Solution10.java
@@ -0,0 +1,14 @@
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums.length==0)return null;
+        return f(nums,0,nums.length-1);
+    }
+    public TreeNode f(int nums[],int l,int h){
+        if(l>h)return null;
+        int mid=(l+h)/2;
+        TreeNode node=new TreeNode(nums[mid]);
+        node.left=f(nums,l,mid-1);
+        node.right=f(nums,mid+1,h);
+        return node;
+    }
+}
