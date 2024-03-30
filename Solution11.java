430bf2911fc647c9b237214a75b9c70dcc000447
Kartik Chaudhary <117715537+KartiKChaudhary07@users.noreply.github.com>
2024-03-30 10:03:53 +0530

    Create Solution11.java

---
+++ Solution11.java
@@ -0,0 +1,35 @@
+/*
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+Example 2:
+
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+Example 3:
+
+Input: nums = [2,1,5,0,4,6]
+Output: true
+Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.*/
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int c = nums[i];
+            if (c <= a) {
+                a = c;
+            } else if (c <= b) {
+                b = c;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
