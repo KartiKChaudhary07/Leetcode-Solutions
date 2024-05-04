00d30e9fc04ba3c6efa130e9298c30f099dd8943
Kartik Chaudhary <117715537+KartiKChaudhary07@users.noreply.github.com>
2024-05-04 21:23:48 +0530

    Create Solution41.java

---
+++ Solution41.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int boats = 0;
+        int left = 0, right = people.length - 1;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            boats++;
+        }
+        return boats;
+    }
+}
