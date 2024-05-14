450fcbe90ec953e80b68b05d6c5bc4e0dc056231
Kartik Chaudhary <117715537+KartiKChaudhary07@users.noreply.github.com>
2024-05-14 15:22:05 +0530

    Create solution50.java

---
+++ solution50.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(0, 0, inorder.length - 1, preorder, inorder);
+    }
+
+    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        if (preStart > preorder.length - 1 || inStart > inEnd) return null;
+
+        TreeNode root = new TreeNode(preorder[preStart]);
+
+        int index = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (root.val == inorder[i]) {
+                index = i;
+                break; 
+            }
+        }
+
+        root.left = helper(preStart + 1, inStart, index - 1, preorder, inorder);
+        root.right = helper(preStart + index - inStart + 1, index + 1, inEnd, preorder, inorder);
+
+        return root;
+    }
+}
