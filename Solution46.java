f73c56291e179b3893d1d7b9588964246b2de0f3
Kartik Chaudhary <117715537+KartiKChaudhary07@users.noreply.github.com>
2024-05-24 14:33:01 +0530

    Create Solution46.java

---
+++ Solution46.java
@@ -0,0 +1,16 @@
+ class Solution {
+    public boolean helper(TreeNode root) {
+        if(root.val == 0 || root.val == 1) {
+            return root.val == 1;
+        } else if(root.val == 2) {
+            return helper(root.left) || helper(root.right);
+        } else if(root.val == 3) {
+            return helper(root.left) && helper(root.right);
+        } 
+
+        return false;
+    }
+    public boolean evaluateTree(TreeNode root) {
+        return helper(root);
+    }
+}
