0398737f914a62c0b23f06effdb5314cf42047eb
Kartik Chaudhary <117715537+KartiKChaudhary07@users.noreply.github.com>
2024-07-08 23:07:39 +0530

    Create Solution65.java

---
+++ Solution65.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        ArrayList<Integer> circle = new ArrayList<>();
+        for (int i = 1; i <= n; ++i) {
+            circle.add(i);
+        }
+        int cur_ind = 0;
+
+        while (circle.size() > 1) {
+            int next_to_remove = (cur_ind + k - 1) % circle.size();
+            circle.remove(next_to_remove);
+            cur_ind = next_to_remove;
+        }
+
+        return circle.get(0);
+    }
+}
