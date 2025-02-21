7138265437075f9b4d233e4570d951cc9ab5641a
Kartik Chaudhary <117715537+KartiKChaudhary07@users.noreply.github.com>
2025-02-21 22:31:46 +0530

    Create 1346. Check If N and Its Double Exist.java

---
+++ 1346. Check If N and Its Double Exist.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
